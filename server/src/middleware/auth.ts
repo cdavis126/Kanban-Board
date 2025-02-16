4e01243dec3db175b1416ada731e08a5a6da988f
Cherie Davis <cheriedavis126@gmail.com>
2025-02-15 23:14:10 -0600

    updating auth-routes.ts

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,10 +1,30 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+const secretKey = process.env.JWT_SECRET_KEY as string;
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction): Response | void => {
+  const authHeader = req.header('Authorization');
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access Denied. No token provided.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secretKey) as JwtPayload;
+    (req as any).user = decoded; // Attach user info to jwt info
+    return next(); // do I need this?
+  } catch (err) {
+    return res.status(403).json({ error: 'Invalid Token' });
+  }
 };
+
+
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -2,12 +2,39 @@ import { Router, Request, Response } from 'express';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
 
-export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
-};
+dotenv.config();
 
 const router = Router();
+const secretKey = process.env.JWT_SECRET_KEY as string;
+
+export const login = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { username, password } = req.body;
+
+    // check status of user and if the user exists
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    // alidate the password using bcrypt
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    //Generate a JWT token for user
+    const token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '2h' });
+
+    // Send token to the cuser
+    return res.json({ token });
+  } catch (error) {
+    console.error('Login Error:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 // POST /login - Login a user
 router.post('/login', login);
