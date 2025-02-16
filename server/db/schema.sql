93b74bd1e1c2de56361c79aa1eb3c25ffd5a9f73
Cherie Davis <cheriedavis126@gmail.com>
2025-02-15 22:28:31 -0600

    SQL and database set up

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,5 +1,16 @@
--- DROP DATABASE
-DROP DATABASE IF EXISTS kanban_db;
+--sql connected and now: create the user and then create the tickets?
+-- Create Users Table
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
 
--- CREATE DATABASE
-CREATE DATABASE kanban_db;
+-- Create Tickets Table
+CREATE TABLE IF NOT EXISTS tickets (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    status VARCHAR(50) NOT NULL,
+    assigned_to INTEGER REFERENCES users(id) ON DELETE SET NULL
+);
